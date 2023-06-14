a79bcf4e006a565c1f961316cfa637e623bcaa7a
Nick Liu <hybrida666@icloud.com>
2023-06-14 20:29:12 +0800

    Airbnb Swift Style Guide

--- LoveDive/LoveDive/ViewController.swift
+++ LoveDive/LoveDive/ViewController.swift
@@ -9,10 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
 
 
 }
