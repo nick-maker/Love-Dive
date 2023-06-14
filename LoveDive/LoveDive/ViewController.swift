6452f579c54959f15b699ba4c8fd12065f4187cf
Nick Liu <hybrida666@icloud.com>
2023-06-14 19:23:07 +0800

    Initial Commit

---
+++ LoveDive/LoveDive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoveDive
+//
+//  Created by Nick Liu on 2023/6/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
