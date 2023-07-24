70f64f53f576e3b1603fae8530f0d0933b9bdb9c
Nick Liu <hybrida666@icloud.com>
2023-07-24 11:02:15 +0800

    Unit Test setup

---
+++ LoveDive/LoveDiveTests/LoveDiveTests.swift
@@ -0,0 +1,15 @@
+//
+//  LoveDiveTests.swift
+//  LoveDiveTests
+//
+//  Created by Nick Liu on 2023/7/21.
+//
+
+import XCTest
+@testable import LoveDive
+
+final class LoveDiveTests: XCTestCase {
+
+
+
+}
